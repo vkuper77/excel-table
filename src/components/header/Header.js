878b1ae1f9ee887bf6e4b0e505e156f26dcc61ab
vkuper77 <vkuper7@gmail.com>
2021-06-27 14:14:31 +0300

    layout formation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,19 @@
 import { ExcelComponent } from '@core/ExcelComponent'
 
 export class Header extends ExcelComponent {
+  static className = 'excel__header'
+
   toHTML() {
-    return '<h1>Header</h1>'
+    return `
+    <input type="text" class="input" value="Новая таблица" />
+    <div>
+      <div class="button">
+        <span class="material-icons"> exit_to_app </span>
+      </div>
+      <div class="button">
+        <span class="material-icons"> delete </span>
+      </div>
+    </div>
+    `
   }
 }
