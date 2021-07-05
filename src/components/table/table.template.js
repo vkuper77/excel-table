ff2f5edf9b36119f13e173ea5c0936f810965e52
vkuper77 <vkuper7@gmail.com>
2021-07-05 21:13:36 +0300

    create cell

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,18 +3,18 @@ const CODES = {
   Z: 90,
 }
 
-function createCel() {
-  return `<div class="cell" contenteditable>A1</div>`
+function toCell() {
+  return `<div class="cell" contenteditable></div>`
 }
 
 function toColumn(col) {
   return ` <div class="column">${col}</div>`
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
   <div class="row">
-    <div class="row-info"></div>
+    <div class="row-info">${index ? index : ''}</div>
     <div class="row-data">${content}</div>
   </div>
   `
@@ -24,16 +24,16 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 5) {
+export function createTable(rowsCount = 50) {
   const colsCount = CODES.Z - CODES.A + 1
-  const rows = []
-
+  const rows = new Array()
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('')
 
-  rows.push(createRow(cols))
+  rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow(i))
+    const cells = new Array(colsCount).fill('').map(toCell).join('')
+    rows.push(createRow(i + 1, cells))
   }
 
   return rows.join('')
