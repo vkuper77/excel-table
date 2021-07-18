ad70482dc34110bd2d439ac59e35ad78f790e31f
vkuper77 <vkuper7@gmail.com>
2021-07-18 19:31:43 +0300

    resize row

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -20,11 +20,26 @@ export class Table extends ExcelComponent {
       const $resizer = $(event.target)
       const $parent = $resizer.closest('[data-type="resizable"]')
       const coords = $parent.getCoords()
+      const type = $resizer.data.resize
+
+      const celss = this.$root.findAll(`[data-col="${$parent.data.col}"`)
 
       document.onmousemove = (e) => {
-        const delta = e.pageX - coords.right
-        const value = coords.width + delta
-        $parent.$el.style.width = value + 'px'
+        if (type === 'col') {
+          const delta = e.pageX - coords.right
+          const value = coords.width + delta
+
+          $parent.css({
+            width: value + 'px',
+          })
+          celss.forEach((el) => (el.style.width = value + 'px'))
+        } else {
+          const delta = e.pageY - coords.bottom
+          const value = coords.height + delta
+          $parent.css({
+            height: value + 'px',
+          })
+        }
       }
 
       document.onmouseup = () => {
